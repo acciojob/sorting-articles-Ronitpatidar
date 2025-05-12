7c1b140f6074fd669bf5b76b00905c3fa6cc2a10
Ronitpatidar <ronitpatidar22@gmail.com>
2025-05-12 13:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const bands = [
   'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
   'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
-  , 'Counterparts', 'Oh, Sleeper', 'An Old Dog', 
-  'Anywhere But Here', 
+	'We Came as Romans', 'Counterparts', 'Oh, Sleeper','A Skylit Drive', 'Anywhere But Here','An Old Dog'  
 ];
 
 function stripArticle(name) {
