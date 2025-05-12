07f7c6826fd4a1989fe81bd1c6bff45f567b15bd
Ronitpatidar <ronitpatidar22@gmail.com>
2025-05-12 13:55:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 const bands = [
   'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
   'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
-	'We Came as Romans', 'Counterparts', 'Oh, Sleeper','A Skylit Drive', 'Anywhere But Here','An Old Dog'  
+  'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+  'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
 ];
 
-function stripArticle(name) {
-  return name.replace(/^(a |an |the )/i, '').trim();
-}
-
-const sortedBands = bands.sort((a, b) => {
-  return stripArticle(a).localeCompare(stripArticle(b));
-});
-
 const bandList = document.getElementById('band');
-sortedBands.forEach(band => {
+bands.forEach(band => {
   const li = document.createElement('li');
   li.textContent = band;
   bandList.appendChild(li);
