6e94df5ff8f81d4b5f4dbd99f2c10bd156010c75
Ronitpatidar <ronitpatidar22@gmail.com>
2025-05-13 08:42:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const bands = [
   'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
 ];
 
-const bandList = document.getElementById('band');
+const bandList = document.getElementById('bands');
 bands.forEach(band => {
   const li = document.createElement('li');
   li.textContent = band;
